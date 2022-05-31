9ece87c126daaae9952064689e91755563d5f1ec
mstmdev <mstmdev@gmail.com>
2022-06-01 01:50:52 +0800

    Add some functions for getting primary data type results

--- extension/extension.go
+++ extension/extension.go
@@ -15,11 +15,47 @@ func New(cache nscache.NSCache) nscache.NSCacheExt {
 	}
 }
 
-func (ext *extension) GetString(k string) (s string, ok bool) {
-	var v *string
-	err := ext.c.Get(k, &v)
-	if err != nil || v == nil {
-		return "", false
+func (ext *extension) GetString(k string) (v string, ok bool) {
+	return getValue[string](k, ext.c.Get)
+}
+
+func (ext *extension) GetBool(k string) (v bool, ok bool) {
+	return getValue[bool](k, ext.c.Get)
+}
+
+func (ext *extension) GetByte(k string) (v byte, ok bool) {
+	return getValue[byte](k, ext.c.Get)
+}
+
+func (ext *extension) GetInt(k string) (v int, ok bool) {
+	return getValue[int](k, ext.c.Get)
+}
+
+func (ext *extension) GetInt64(k string) (v int64, ok bool) {
+	return getValue[int64](k, ext.c.Get)
+}
+
+func (ext *extension) GetFloat32(k string) (v float32, ok bool) {
+	return getValue[float32](k, ext.c.Get)
+}
+
+func (ext *extension) GetFloat64(k string) (v float64, ok bool) {
+	return getValue[float64](k, ext.c.Get)
+}
+
+func (ext *extension) GetComplex64(k string) (v complex64, ok bool) {
+	return getValue[complex64](k, ext.c.Get)
+}
+
+func (ext *extension) GetComplex128(k string) (v complex128, ok bool) {
+	return getValue[complex128](k, ext.c.Get)
+}
+
+func getValue[T any](k string, get func(k string, v any) error) (v T, ok bool) {
+	var p *T
+	err := get(k, &p)
+	if err != nil || p == nil {
+		return v, false
 	}
-	return *v, true
+	return *p, true
 }
--- nscache_ext.go
+++ nscache_ext.go
@@ -3,5 +3,21 @@ package nscache
 // NSCacheExt the NSCache extension function collection
 type NSCacheExt interface {
 	// GetString get string cache data by key
-	GetString(k string) (s string, ok bool)
+	GetString(k string) (v string, ok bool)
+	// GetBool get bool cache data by key
+	GetBool(k string) (v bool, ok bool)
+	// GetByte get byte cache data by key
+	GetByte(k string) (v byte, ok bool)
+	// GetInt get int cache data by key
+	GetInt(k string) (v int, ok bool)
+	// GetInt64 get int64 cache data by key
+	GetInt64(k string) (v int64, ok bool)
+	// GetFloat32 get float32 cache data by key
+	GetFloat32(k string) (v float32, ok bool)
+	// GetFloat64 get float64 cache data by key
+	GetFloat64(k string) (v float64, ok bool)
+	// GetComplex64 get complex64 cache data by key
+	GetComplex64(k string) (v complex64, ok bool)
+	// GetComplex128 get complex128 cache data by key
+	GetComplex128(k string) (v complex128, ok bool)
 }
