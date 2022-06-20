2bd3b6dde99a321496f54015878420f2dc21b9bd
mstmdev <mstmdev@gmail.com>
2022-06-20 23:27:40 +0800

    Add some extension functions for builtin data type

--- extension/extension.go
+++ extension/extension.go
@@ -17,24 +17,36 @@ func New(cache nscache.NSCache) nscache.NSCacheExt {
 	}
 }
 
-func (ext *extension) GetString(k string) (v string, ok bool) {
-	return getValue[string](k, ext.c.Get)
-}
-
-func (ext *extension) GetStringList(k string) (v []string, ok bool) {
-	return getValue[[]string](k, ext.c.Get)
-}
-
 func (ext *extension) GetBool(k string) (v bool, ok bool) {
 	return getValue[bool](k, ext.c.Get)
 }
 
-func (ext *extension) GetByte(k string) (v byte, ok bool) {
-	return getValue[byte](k, ext.c.Get)
+func (ext *extension) GetUint8(k string) (v uint8, ok bool) {
+	return getValue[uint8](k, ext.c.Get)
 }
 
-func (ext *extension) GetInt(k string) (v int, ok bool) {
-	return getValue[int](k, ext.c.Get)
+func (ext *extension) GetUint16(k string) (v uint16, ok bool) {
+	return getValue[uint16](k, ext.c.Get)
+}
+
+func (ext *extension) GetUint32(k string) (v uint32, ok bool) {
+	return getValue[uint32](k, ext.c.Get)
+}
+
+func (ext *extension) GetUint64(k string) (v uint64, ok bool) {
+	return getValue[uint64](k, ext.c.Get)
+}
+
+func (ext *extension) GetInt8(k string) (v int8, ok bool) {
+	return getValue[int8](k, ext.c.Get)
+}
+
+func (ext *extension) GetInt16(k string) (v int16, ok bool) {
+	return getValue[int16](k, ext.c.Get)
+}
+
+func (ext *extension) GetInt32(k string) (v int32, ok bool) {
+	return getValue[int32](k, ext.c.Get)
 }
 
 func (ext *extension) GetInt64(k string) (v int64, ok bool) {
@@ -55,6 +67,42 @@ func (ext *extension) GetComplex64(k string) (v complex64, ok bool) {
 
 func (ext *extension) GetComplex128(k string) (v complex128, ok bool) {
 	return getValue[complex128](k, ext.c.Get)
+}
+
+func (ext *extension) GetString(k string) (v string, ok bool) {
+	return getValue[string](k, ext.c.Get)
+}
+
+func (ext *extension) GetStrings(k string) (v []string, ok bool) {
+	return getValue[[]string](k, ext.c.Get)
+}
+
+func (ext *extension) GetInt(k string) (v int, ok bool) {
+	return getValue[int](k, ext.c.Get)
+}
+
+func (ext *extension) GetInts(k string) (v []int, ok bool) {
+	return getValue[[]int](k, ext.c.Get)
+}
+
+func (ext *extension) GetUint(k string) (v uint, ok bool) {
+	return getValue[uint](k, ext.c.Get)
+}
+
+func (ext *extension) GetUintptr(k string) (v uintptr, ok bool) {
+	return getValue[uintptr](k, ext.c.Get)
+}
+
+func (ext *extension) GetByte(k string) (v byte, ok bool) {
+	return getValue[byte](k, ext.c.Get)
+}
+
+func (ext *extension) GetBytes(k string) (v []byte, ok bool) {
+	return getValue[[]byte](k, ext.c.Get)
+}
+
+func (ext *extension) GetRune(k string) (v rune, ok bool) {
+	return getValue[rune](k, ext.c.Get)
 }
 
 func (ext *extension) GetTime(k string) (v time.Time, ok bool) {
--- nscache_ext.go
+++ nscache_ext.go
@@ -4,16 +4,22 @@ import "time"
 
 // NSCacheExt the NSCache extension function collection
 type NSCacheExt interface {
-	// GetString get string cache data by key
-	GetString(k string) (v string, ok bool)
-	// GetStringList get string list cache data by key
-	GetStringList(k string) (v []string, ok bool)
 	// GetBool get bool cache data by key
 	GetBool(k string) (v bool, ok bool)
-	// GetByte get byte cache data by key
-	GetByte(k string) (v byte, ok bool)
-	// GetInt get int cache data by key
-	GetInt(k string) (v int, ok bool)
+	// GetUint8 get uint8 cache data by key
+	GetUint8(k string) (v uint8, ok bool)
+	// GetUint16 get uint16 cache data by key
+	GetUint16(k string) (v uint16, ok bool)
+	// GetUint32 get uint32 cache data by key
+	GetUint32(k string) (v uint32, ok bool)
+	// GetUint64 get uint64 cache data by key
+	GetUint64(k string) (v uint64, ok bool)
+	// GetInt8 get int8 cache data by key
+	GetInt8(k string) (v int8, ok bool)
+	// GetInt16 get int16 cache data by key
+	GetInt16(k string) (v int16, ok bool)
+	// GetInt32 get int32 cache data by key
+	GetInt32(k string) (v int32, ok bool)
 	// GetInt64 get int64 cache data by key
 	GetInt64(k string) (v int64, ok bool)
 	// GetFloat32 get float32 cache data by key
@@ -24,6 +30,24 @@ type NSCacheExt interface {
 	GetComplex64(k string) (v complex64, ok bool)
 	// GetComplex128 get complex128 cache data by key
 	GetComplex128(k string) (v complex128, ok bool)
+	// GetString get string cache data by key
+	GetString(k string) (v string, ok bool)
+	// GetStrings get string list cache data by key
+	GetStrings(k string) (v []string, ok bool)
+	// GetInt get int cache data by key
+	GetInt(k string) (v int, ok bool)
+	// GetInts get int list cache data by key
+	GetInts(k string) (v []int, ok bool)
+	// GetUint get uint cache data by key
+	GetUint(k string) (v uint, ok bool)
+	// GetUintptr get uintptr cache data by key
+	GetUintptr(k string) (v uintptr, ok bool)
+	// GetByte get byte cache data by key
+	GetByte(k string) (v byte, ok bool)
+	// GetBytes get byte list cache data by key
+	GetBytes(k string) (v []byte, ok bool)
+	// GetRune get rune cache data by key
+	GetRune(k string) (v rune, ok bool)
 	// GetTime get time.Time cache data by key
 	GetTime(k string) (v time.Time, ok bool)
 	// GetDuration get time.Duration cache data by key
