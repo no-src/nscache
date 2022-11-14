bcd9d06bc0c9bc579fd1b155e3d47cd701120960
mstmdev <mstmdev@gmail.com>
2022-11-15 00:54:52 +0800

    Update some comments in the testutil.go

--- internal/testutil/testutil.go
+++ internal/testutil/testutil.go
@@ -3,10 +3,14 @@ package testutil
 import "time"
 
 const (
+	// MemoryConnectionString a memory cache driver test connection string
 	MemoryConnectionString = "memory:"
+	// BuntDBConnectionString a buntdb cache driver test connection string
 	BuntDBConnectionString = "buntdb://:memory:"
-	EtcdConnectionString   = "etcd://127.0.0.1:2379?dial_timeout=5s"
-	RedisConnectionString  = "redis://127.0.0.1:6379"
-
+	// EtcdConnectionString a etcd cache driver test connection string
+	EtcdConnectionString = "etcd://127.0.0.1:2379?dial_timeout=5s"
+	// RedisConnectionString a redis cache driver test connection string
+	RedisConnectionString = "redis://127.0.0.1:6379"
+	// DefaultExpiration the default expiration time for cache driver tests
 	DefaultExpiration = time.Minute
 )
