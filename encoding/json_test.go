cb4207bc046d65d0386db1043a6e3f6f100b52c5
mstmdev <mstmdev@gmail.com>
2022-11-20 09:07:05 +0800

    Add some tests for the encoding package

---
+++ encoding/json_test.go
@@ -0,0 +1,64 @@
+package encoding
+
+import (
+	"testing"
+	"time"
+)
+
+func TestJson_Serialize(t *testing.T) {
+	s := &jsonSerializer{}
+	ms := getMyStructCase()
+	data, err := s.Serialize(ms)
+	if err != nil {
+		t.Errorf("json serialize error, err=%v", err)
+		return
+	}
+	expect := ms.json()
+	actual := string(data)
+	if actual != expect {
+		t.Errorf("json serialize error, expect to get %s, but get %s", expect, actual)
+	}
+}
+
+func TestJson_Deserialize(t *testing.T) {
+	s := &jsonSerializer{}
+	ms := getMyStructCase()
+	var target MyStruct
+	err := s.Deserialize([]byte(ms.json()), &target)
+	if err != nil {
+		t.Errorf("json deserialize error, err=%v", err)
+		return
+	}
+	if !ms.equal(target) {
+		t.Errorf("json deserialize error, current case is not equal to target")
+	}
+}
+
+type MyStruct struct {
+	Name       string        `json:"name"`
+	ID         int64         `json:"id"`
+	IsValid    bool          `json:"is_valid"`
+	CreateTime time.Time     `json:"create_time"`
+	Expires    time.Duration `json:"expires"`
+
+	jsonData string
+}
+
+func (ms MyStruct) json() string {
+	return ms.jsonData
+}
+
+func (ms MyStruct) equal(target MyStruct) bool {
+	return ms.Name == target.Name && ms.ID == target.ID && ms.CreateTime == target.CreateTime && ms.Expires == target.Expires
+}
+
+func getMyStructCase() MyStruct {
+	return MyStruct{
+		Name:       "go",
+		ID:         1,
+		IsValid:    true,
+		CreateTime: time.Unix(1669323967, 0),
+		Expires:    time.Minute * 3,
+		jsonData:   `{"name":"go","id":1,"is_valid":true,"create_time":"2022-11-25T05:06:07+08:00","expires":180000000000}`,
+	}
+}
