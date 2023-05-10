0466b20e49d2d167a1fb5ed56c8d463aef764392
mstmdev <mstmdev@gmail.com>
2023-05-11 01:11:49 +0800

    Fix the store package

--- store/data.go
+++ store/data.go
@@ -7,18 +7,26 @@ type Data struct {
 	// Data the real user data
 	Data []byte
 	// ExpireTime the expiration time of the data
-	ExpireTime time.Time
+	ExpireTime *time.Time
 }
 
 // NewData create a store data object
 func NewData(data []byte, expiration time.Duration) *Data {
+	var pt *time.Time
+	if expiration > 0 {
+		t := time.Now().Add(expiration)
+		pt = &t
+	}
 	return &Data{
 		Data:       data,
-		ExpireTime: time.Now().Add(expiration),
+		ExpireTime: pt,
 	}
 }
 
 // IsExpired check if the data is expired or not
 func (d *Data) IsExpired() bool {
+	if d.ExpireTime == nil {
+		return false
+	}
 	return d.ExpireTime.Before(time.Now())
 }
