ba8d55930d18f414c48df39f803a69fa6f5d865c
mstmdev <mstmdev@gmail.com>
2024-01-17 09:23:30 +0800

    Add the all package to register all cache drivers

---
+++ all/all.go
@@ -0,0 +1,11 @@
+package all
+
+import (
+	_ "github.com/no-src/nscache/bigcache"
+	_ "github.com/no-src/nscache/buntdb"
+	_ "github.com/no-src/nscache/etcd"
+	_ "github.com/no-src/nscache/fastcache"
+	_ "github.com/no-src/nscache/freecache"
+	_ "github.com/no-src/nscache/memory"
+	_ "github.com/no-src/nscache/redis"
+)
